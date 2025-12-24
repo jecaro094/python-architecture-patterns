430a64f4f7390880ec7a7001944d7a15a3403274
jecaro094 <jecarobd@gmail.com>
2025-12-24 18:12:29 +0100

    feat: Improve existing code and add new functionality
    - Store a line from different batches
    - Comments for improvement of domain code
    - Comparaison ENTITY VS VALUE OBJECT

--- domain.py
+++ domain.py
@@ -1,17 +1,35 @@
+from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import List, Optional, Set
 from uuid import UUID, uuid4
 
 import constants as const
 import exceptions as ex
 
-
 # @dataclass(frozen=True) # solo sirve si se usan atributos de clase, y no de objeto
+
+
+# NOTE This was my first approach, which is not bad
+# NOTE But this is an ENTITY and not a VALUE OBJECT (take into account...)
 class OrderLine:
-    def __init__(self, sku, quantity, reference: UUID = uuid4()):
+    def __init__(self, sku, quantity):
+        self.reference = uuid4()
         self.sku = sku
         self.quantity = quantity
-        self.reference = reference
+
+
+# NOTE This is a VALUE OBJECT: those identified uniquely by the data it holds (in its attributes)
+# @dataclass(frozen=True)
+# class OrderLine:
+#     sku: str
+#     quantity: int
+#     reference: UUID # We have to provide the unique uuid4() in its instantiation
+
+# Example: order_line = OrderLine(reference=uuid4(), sku="SMALL-TABLE", quantity=40)
+
+# NOTE
+# ENTITY: LONG LIVE IDENTITY
+# VALUE OBJECT: IDENTIFIED BY ITS DATA
 
 
 class Batch:
@@ -19,15 +37,15 @@ class Batch:
         self,
         sku,
         quantity,
-        orders: set = set(),
+        orders: Set[OrderLine] = set(),
         eta: Optional[date] = None,
-        reference: UUID = uuid4(),
+        # reference: UUID = uuid4(),  # NOTE Danger!! if we put `reference: UUID = uuid4()` the value is fixed for all objects created!!
     ):
+        self.reference = uuid4()
         self.sku = sku
         self.quantity = quantity
         self.orders = orders
         self.eta = eta
-        self.reference = reference
 
     def __repr__(self):
         return (
@@ -47,10 +65,47 @@ class Batch:
         if not self.can_deallocate(order_line):
             raise ex.AllocationException(const.DEALLOCATE_ERROR_MSG)
 
-        self.orders.clear(order_line)
+        self.orders.remove(order_line)
+        self.quantity += order_line.quantity
 
     def can_deallocate(self, order_line: OrderLine):
         return order_line in self.orders
 
     def can_allocate(self, order_line: OrderLine):
         return order_line.sku == self.sku and order_line not in self.orders
+
+    # NOTE Mine (first version, not that bad)
+    # def __gt__(self, other):
+    #     if not isinstance(other, Batch):
+    #         return False
+    #     if self_eta := self.eta :
+    #         return self_eta > other_eta if (other_eta := other.eta) else True
+    #     return False
+
+    # NOTE Book version (more legible than mine)
+    def __gt__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        if (self_eta := self.eta) is None:
+            return False
+        if (other_eta := other.eta) is None:
+            return True
+
+        return self_eta > other_eta
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> UUID:
+    """
+    Given a list of batches in `batches`, this functions allocates the given 'line'
+    in the earliest batch if possible.
+    """
+
+    # NOTE Mine (could improve)
+    # ordered_batches: List[Batch] = sorted(batches)
+    # priority_batch = next(iter(ordered_batches))
+
+    # NOTE Improvement from the book
+    priority_batch = next(b for b in sorted(batches) if b.can_allocate(line))
+
+    priority_batch.allocate(order_line=line)
+    return priority_batch.reference
--- test_domain.py
+++ test_domain.py
@@ -1,10 +1,14 @@
 # from unittest.mock import MagicMock, patch
 
+from datetime import datetime
+
 import pytest
 
 import constants as const
 import exceptions as ex
-from domain import Batch, OrderLine
+from domain import Batch, OrderLine, allocate
+
+# NOTE Tests basic domain behaviour
 
 
 def test_allocate_in_batch_order_line_greater():
@@ -41,7 +45,7 @@ def test_allocate_in_batch_order_line_twice():
     assert batch.quantity == 18
 
 
-def test_allocate_different_sku():
+def test_allocate_with_different_sku():
     batch = Batch(sku="SMALL-TABLE", quantity=20)
     order_line = OrderLine(sku="LARGE-TABLE", quantity=2)
 
@@ -51,4 +55,91 @@ def test_allocate_different_sku():
     assert str(exc_info.value) == const.ALLOCATE_ERROR_MSG
 
 
-# TODO Deallocate to be tested...
+def test_deallocate_order_line_from_batch():
+    batch = Batch(sku="SMALL-TABLE", quantity=20)
+    order_line = OrderLine(sku="SMALL-TABLE", quantity=2)
+    batch.allocate(order_line)
+    batch.deallocate(order_line)
+    assert batch.quantity == 20
+
+
+def test_deallocate_order_line_not_from_batch():
+    batch = Batch(sku="SMALL-TABLE", quantity=20)
+    order_line = OrderLine(sku="SMALL-TABLE", quantity=2)
+    batch.allocate(order_line)
+
+    with pytest.raises(ex.AllocationException) as exc_info:
+        batch.deallocate(OrderLine(sku="LARGE-TABLE", quantity=2))
+
+    assert str(exc_info.value) == const.DEALLOCATE_ERROR_MSG
+    assert batch.quantity == 18
+
+
+# NOTE Tests for greater than in batch
+
+
+def test_batch_with_none_eta_less_than_batch_with_eta():
+    """
+    We check that batch with `None` has priority.
+    """
+    batch_with_none = Batch(sku="SMALL-TABLE", quantity=20)
+    batch_date = Batch(sku="SMALL-TABLE", quantity=20, eta=datetime(2026, 11, 5))
+    assert batch_with_none < batch_date
+
+
+def test_batches_with_different_etas_comparaison():
+    """
+    We check that batch with lesser date has priority
+    """
+    batch_earlier = Batch(sku="SMALL-TABLE", quantity=20, eta=datetime(2026, 11, 5))
+    batch_later = Batch(sku="SMALL-TABLE", quantity=20, eta=datetime(2027, 11, 5))
+    assert batch_earlier < batch_later
+
+
+# NOTE Tests domain function behaviour
+
+
+def test_allocate_line_in_batches_minor_date():
+    """
+    Supposed sames SKUs in all batches, to prevent exceptions
+    """
+    batch_earlier = Batch(sku="SMALL-TABLE", quantity=20, eta=datetime(2026, 11, 5))
+    batch_later = Batch(sku="BIG-TABLE", quantity=20, eta=datetime(2027, 11, 5))
+    order_line = OrderLine(sku="SMALL-TABLE", quantity=2)
+
+    allocation = allocate(order_line, [batch_earlier, batch_later])
+    assert batch_earlier.quantity == 18
+    assert batch_later.quantity == 20
+
+    assert allocation == batch_earlier.reference
+
+
+def test_allocate_line_in_batches_none_in_single_batch():
+    """
+    Supposed sames SKUs in all batches, to prevent exceptions
+    """
+    batch_date = Batch(sku="SMALL-TABLE", quantity=20, eta=datetime(2026, 11, 5))
+    batch_none = Batch(sku="SMALL-TABLE", quantity=20)
+    order_line = OrderLine(sku="SMALL-TABLE", quantity=2)
+
+    allocation = allocate(order_line, [batch_date, batch_none])
+    assert batch_date.quantity == 20
+    assert batch_none.quantity == 18
+
+    assert allocation == batch_none.reference
+
+
+def test_allocate_line_in_batch_with_same_sku():
+    batch_different_sku_earlier = Batch(
+        sku="LARGE-TABLE", quantity=20, eta=datetime(2024, 11, 5)
+    )
+    batch_same_sku_later = Batch(
+        sku="SMALL-TABLE", quantity=20, eta=datetime(2026, 11, 5)
+    )
+    order_line = OrderLine(sku="SMALL-TABLE", quantity=2)
+
+    allocation = allocate(order_line, [batch_different_sku_earlier, batch_same_sku_later])
+    assert batch_different_sku_earlier.quantity == 20
+    assert batch_same_sku_later.quantity == 18
+
+    assert allocation == batch_same_sku_later.reference
